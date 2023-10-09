420935b89e3d064bc0a05f83800ce85f184c817d
iSmartCoding <ismartcoding@gmail.com>
2023-10-09 15:48:37 +0800

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">Free Currency Rates API</h1>
+
+[![](https://data.jsdelivr.com/v1/package/gh/ismartcoding/currency-api/badge)](https://www.jsdelivr.com/package/gh/ismartcoding/currency-api)
+[![](https://data.jsdelivr.com/v1/package/gh/ismartcoding/currency-api/badge/rank)](https://www.jsdelivr.com/package/gh/ismartcoding/currency-api)
+[![Fetch-Currencies](https://github.com/ismartcoding/currency-api/actions/workflows/run.yml/badge.svg)](https://github.com/ismartcoding/currency-api/actions/workflows/run.yml)
+
+**Features:**
+- Free & Blazing Fast response
+- No Rate limits
+- 169 Currencies
+- Daily Updated
+
+
+**API URL:**
+
+`https://cdn.jsdelivr.net/gh/ismartcoding/currency-api@main/latest/data.json`
+
+
+### Any Issues: [Raise here](https://github.com/ismartcoding/currency-api/issues/new "Raise here")
+
+Please Star this repo by clicking on [:star: button](#) above [:arrow_upper_right:](#)
+
---
+++ run.js
@@ -0,0 +1,79 @@
+const http = require('http');
+const fs = require('fs');
+
+// Function to get the current date in the 'YYYY-MM-DD' format
+function getCurrentDate() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+// Function to get the current hour in 24-hour format
+function getCurrentHour() {
+  const now = new Date();
+  return String(now.getHours()).padStart(2, '0');
+}
+
+// Function to create a folder by date
+function createFolderByDate(date) {
+  const folderName = `./${date}`;
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+  return folderName;
+}
+
+function fetchAndSaveCurrencyData() {
+  const currentDate = getCurrentDate();
+  const currentHour = getCurrentHour();
+  const folderName = createFolderByDate(currentDate);
+  const fileName = `${currentHour}.json`;
+  const filePath = `${folderName}/${fileName}`;
+
+  const request = http.get(process.env.currlink, (response) => {
+    if (response.statusCode !== 200) {
+      console.error(`HTTP error! Status: ${response.statusCode}`);
+      return;
+    }
+
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      try {
+        const jsonData = JSON.parse(data);
+        const quotes = {}
+        for (const key in jsonData.quotes) {
+          const currency = key.substring(3);
+          quotes[currency] = jsonData.quotes[key];
+        }
+
+        const json = JSON.stringify({
+          ts: jsonData.timestamp,
+          base: jsonData.source,
+          quotes,
+        })
+
+        // Save the JSON data to the file with the hour in the filename
+        fs.writeFileSync(filePath, json);
+        fs.writeFileSync('latest/data.json', json);
+
+        console.log(`Currency data saved to ${filePath}`);
+      } catch (error) {
+        console.error('Error parsing JSON:', error.message);
+      }
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error('Error making HTTP request:', error.message);
+  });
+}
+
+// Call the function to fetch and save data
+fetchAndSaveCurrencyData();
